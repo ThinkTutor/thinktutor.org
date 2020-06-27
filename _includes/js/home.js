8c28768d0df74b41ecccd1ecf0c4a71655a14317
Matthew Thomas <matthew@mwt.me>
2020-06-27 09:39:35 -0400

    do mobile nav in pure css

--- _includes/js/home.js
+++ _includes/js/home.js
@@ -12,17 +12,6 @@
 		}
 	});
 
-	/* Window Resize Mobile Menu Fix */
-	mobileNav();
-
-	/* Menu Dropdown Toggle */
-	if ($('.menu-trigger').length) {
-		$(".menu-trigger").on('click', function () {
-			$(this).toggleClass('active');
-			$('.header-area .nav').slideToggle(200);
-		});
-	}
-
 	$(document).ready(function () {
 		$(document).on("scroll", onScroll);
 	});
@@ -38,22 +27,6 @@
 			}
 			else {
 				currLink.removeClass("active");
-			}
-		});
-	}
-
-	/* Window Resize Mobile Menu Fix */
-	$(window).on('resize', function () {
-		mobileNav();
-	});
-
-	/* Window Resize Mobile Menu Fix */
-	function mobileNav() {
-		var width = $(window).width();
-		$('.submenu').on('click', function () {
-			if (width < 767) {
-				$('.submenu ul').removeClass('active');
-				$(this).find('ul').toggleClass('active');
 			}
 		});
 	}
