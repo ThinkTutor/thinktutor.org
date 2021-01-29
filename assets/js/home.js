24ff50a858e55323efafd3eeb6b8a86d0179c271
Matthew Thomas <matthew@mwt.me>
2021-01-29 17:28:22 -0600

    replace mail form with homemade mail-form

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,4 @@
+/* --- Highlight nav links on scroll --- */
 window.onscroll = onScroll;
 
 function onScroll(event) {
@@ -11,7 +12,7 @@ function onScroll(event) {
 		header.classList.add('background-header');
 	} else {
 		header.classList.remove('background-header');
-	}
+	};
 	/* highlight active section links */
 	var scroll_adj = scrollPos + 100;
 	var navLinks = document.querySelectorAll('.nav .scroll-to-section a');
@@ -24,4 +25,50 @@ function onScroll(event) {
 			navLink.classList.remove('active');
 		}
 	});
-}
+};
+
+/* --- submit form --- */
+
+var endpoint = "https://nnmb1j35mg.execute-api.us-east-1.amazonaws.com/default/thinktutor";
+function buildJsonFormData(form) {
+	const jsonFormData = {};
+	for (const pair of new FormData(form)) {
+		jsonFormData[pair[0]] = pair[1];
+	};
+	return jsonFormData;
+};
+var form = document.getElementById("contact");
+var formMessage = document.getElementById("form-message");
+var lockModal = document.getElementById("lock-modal");
+onSubmit = function (token) {
+	/* get data from form */
+	var formData = new buildJsonFormData(form);
+	if (formData["h-captcha-response"] == "") {
+		hcaptcha.reset();
+		hcaptcha.execute();
+	} else {
+		/* lock the form */
+		lockModal.classList.remove("d-none");
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", endpoint, true);
+		xhr.onload = function (e) {
+			var response = JSON.parse(xhr.response);
+			if (xhr.status === 200) {
+				hcaptcha.reset();
+				form.reset();
+				formMessage.classList.remove("d-none");
+				formMessage.classList.add("alert-success");
+				formMessage.innerHTML = "Success! We have sent you an email.";
+			} else {
+				hcaptcha.reset();
+				formMessage.classList.remove("d-none");
+				formMessage.classList.add("alert-warning");
+				formMessage.innerHTML = "Error: " + response.error;
+			};
+			/* Unlock the form */
+			lockModal.classList.add("d-none");
+		};
+		xhr.setRequestHeader('Content-Type', 'text/plain');
+		xhr.send(JSON.stringify(formData));
+	};
+};
